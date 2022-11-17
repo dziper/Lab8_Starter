267c42f932ea204185584cdda366b2366b8dc5ab
dziper <dziper@me.com>
2022-11-17 09:56:33 -0800

    add package.json, do Step 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Lab 8 - Starter
+1) I would fit my automated tests within a Github Action that runs automatically. That way I don't have to be worried about forgetting to test when pushing code, and other contributors' code runs the same tests as mine.
+2) No
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -20,18 +20,22 @@ describe('Basic user flow for Website', () => {
     console.log('Checking to make sure <product-item> elements are populated...');
     // Start as true, if any don't have data, swap to false
     let allArePopulated = true;
-    let data, plainValue;
-    // Query select all of the <product-item> elements
     const prodItems = await page.$$('product-item');
-    console.log(`Checking product item 1/${prodItems.length}`);
-    // Grab the .data property of <product-items> to grab all of the json data stored inside
-    data = await prodItems[0].getProperty('data');
-    // Convert that property to JSON
-    plainValue = await data.jsonValue();
-    // Make sure the title, price, and image are populated in the JSON
-    if (plainValue.title.length == 0) { allArePopulated = false; }
-    if (plainValue.price.length == 0) { allArePopulated = false; }
-    if (plainValue.image.length == 0) { allArePopulated = false; }
+
+    for (let i = 0; i < prodItems.length; i++) {
+      let data, plainValue;
+      // Query select all of the <product-item> elements
+      console.log(`Checking product item 1/${prodItems.length}`);
+      // Grab the .data property of <product-items> to grab all of the json data stored inside
+      data = await prodItems[1].getProperty('data');
+      // Convert that property to JSON
+      plainValue = await data.jsonValue();
+      // Make sure the title, price, and image are populated in the JSON
+      if (plainValue.title.length == 0) { allArePopulated = false; }
+      if (plainValue.price.length == 0) { allArePopulated = false; }
+      if (plainValue.image.length == 0) { allArePopulated = false; }
+    }
+    
     // Expect allArePopulated to still be true
     expect(allArePopulated).toBe(true);
 
