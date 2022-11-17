ced29c806c70d6bb4d07853df30ebf9ded6d66b9
dziper <dziper@me.com>
2022-11-17 11:49:18 -0800

    finish explore, add screenshot

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Lab 8 - Starter
+Ella Tung
+Daniel Ziper
+
 1) I would fit my automated tests within a Github Action that runs automatically. That way I don't have to be worried about forgetting to test when pushing code, and other contributors' code runs the same tests as mine.
 2) No
 3) No I would not. Testing the message feature of the application would likely require a more complex test than a simple unit test testing just one function, since there are multiple interacting users.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,7 +2,7 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// email
 test('check regular email', () => {
     expect(functions.isEmail("abc@gmail.com")).toBe(true);
 });
@@ -19,6 +19,7 @@ test('check bad email ending', () => {
     expect(functions.isEmail("abc@gmail.comsad")).toBe(false);
 });
 
+// phone
 test('check regular phone', () => {
     expect(functions.isPhoneNumber("123-232-2232")).toBe(true);
 });
@@ -33,4 +34,44 @@ test('check bad phone no hyphen', () => {
 
 test('check phone ext', () => {
     expect(functions.isPhoneNumber("123-232-223212")).toBe(true);
+});
+
+// strongPassword
+test('checks strong password 1', () => {
+    expect(functions.isStrongPassword('Mypassword14_')).toBe(true);
+});
+test('checks strong password 2', () => {
+    expect(functions.isStrongPassword('Hey124__')).toBe(true);
+});
+test('checks bad password 1', () => {
+    expect(functions.isStrongPassword('1234')).toBe(false);
+});
+test('checks bad password 2', () => {
+    expect(functions.isStrongPassword('thispasswordistoolong')).toBe(false);
+});
+// isDate
+test('checks valid date 1', () => {
+    expect(functions.isDate('11/23/2002')).toBe(true);
+});
+test('checks valid date 2', () => {
+    expect(functions.isDate('01/03/1999')).toBe(true);
+});
+test('checks invalid date 1', () => {
+    expect(functions.isDate('01/03/199')).toBe(false);
+});
+test('checks invalid date 2', () => {
+    expect(functions.isDate('1//1999')).toBe(false);
+});
+// isHexColor
+test('checks valid hex color', () => {
+    expect(functions.isHexColor('FF5733')).toBe(true);
+});
+test('checks valid hex color 2', () => {
+    expect(functions.isHexColor('CA4D32')).toBe(true);
+});
+test('checks invalid hex color 1', () => {
+    expect(functions.isHexColor('A')).toBe(false);
+});
+test('checks invalid hex color 2', () => {
+    expect(functions.isHexColor('BB')).toBe(false);
 });
