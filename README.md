67630b76bd94745248c3be1def221f238af2c9e6
dziper <dziper@me.com>
2022-11-17 11:45:33 -0800

    Start explore

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 8 - Starter
 1) I would fit my automated tests within a Github Action that runs automatically. That way I don't have to be worried about forgetting to test when pushing code, and other contributors' code runs the same tests as mine.
 2) No
+3) No I would not. Testing the message feature of the application would likely require a more complex test than a simple unit test testing just one function, since there are multiple interacting users.
+4) Yes I would. The max message length is just one function that could be easily tested on a variety of inputs without relying on other aspects of the app.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,34 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('check regular email', () => {
+    expect(functions.isEmail("abc@gmail.com")).toBe(true);
+});
+
+test('check bad email no @', () => {
+    expect(functions.isEmail("abcgmail.com")).toBe(false);
+});
+
+test('check long email', () => {
+    expect(functions.isEmail("abclaksjdhfaskjdhflkashdflkjhdsaflh@gmaildafasdfasdfasdf.com")).toBe(true);
+});
+
+test('check bad email ending', () => {
+    expect(functions.isEmail("abc@gmail.comsad")).toBe(false);
+});
+
+test('check regular phone', () => {
+    expect(functions.isPhoneNumber("123-232-2232")).toBe(true);
+});
+
+test('check bad abc phone', () => {
+    expect(functions.isPhoneNumber("123-abc-2232")).toBe(false);
+});
+
+test('check bad phone no hyphen', () => {
+    expect(functions.isPhoneNumber("1232222232")).toBe(false);
+});
+
+test('check phone ext', () => {
+    expect(functions.isPhoneNumber("123-232-223212")).toBe(true);
+});
